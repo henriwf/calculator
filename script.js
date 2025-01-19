626bfff12807d39aad1746297b67138bd0a44ee8
henriwf <henriwolfgangf@gmail.com>
2025-01-19 15:29:11 +0000

    create eventListener to display button's textContent save each button's number or operator in a variable create eventListener for equal button to the correct operation using the variables for each number and operator

--- script.js
+++ script.js
@@ -15,15 +15,63 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-let firstNum;
-let secNum;
-let operator;
+let num1;
+let num2;
+let op;
+let isResult = false;
 
 function operate (num1, num2, op) {
 
-    if (op == "+") add(num1, num2);
-    if (op == "-") subtract(num1, num2);
-    if (op == "*") multiply(num1, num2);
-    if (op == "/") divide(num1, num2);
+    if (op == "+") return add(num1, num2);
+    if (op == "-") return subtract(num1, num2);
+    if (op == "*") return multiply(num1, num2);
+    if (op == "/") return divide(num1, num2);
     
 }
+
+
+
+const buttons = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".op");
+const display = document.querySelector(".display");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        if (isResult) {
+            display.textContent = button.textContent;
+            isResult = false;
+
+        } else if (display.textContent == "0" || display.textContent == "+"|| display.textContent == "-" || 
+            display.textContent == "*" || display.textContent == "/") {
+            display.textContent = button.textContent;
+
+        } else {
+            display.textContent += button.textContent;
+        }
+
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener ("click", () => {
+        num1 = parseFloat(display.textContent);
+        display.textContent = operator.textContent;
+        op = operator.textContent;
+    })
+});
+
+
+equal.addEventListener ("click", () => {
+    num2 = parseFloat(display.textContent);
+    let result = operate(num1, num2, op);
+    display.textContent = result;
+    isResult = true;
+});
+
+clear.addEventListener ("click", () => {
+    display.textContent = "0";
+
+});
