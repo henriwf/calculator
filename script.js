2fed1cbcbe194b1c44bdb9d65a45c42f5fa32c6f
henriwf <henriwolfgangf@gmail.com>
2025-01-19 02:45:39 +0000

    create divs for each number, operator and =

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
+
+let firstNum;
+let secNum;
+let operator;
+
+function operate (num1, num2, op) {
+
+    if (op == "+") add(num1, num2);
+    if (op == "-") subtract(num1, num2);
+    if (op == "*") multiply(num1, num2);
+    if (op == "/") divide(num1, num2);
+    
+}
